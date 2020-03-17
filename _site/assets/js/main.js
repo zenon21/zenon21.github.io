c376e5ea4ff10371cff350c2eb904b95ff820919
Cate <ceh2181@columbia.edu>
2020-03-17 15:04:11 -0400

    Source js from separate file

---
+++ _site/assets/js/main.js
@@ -0,0 +1,24 @@
+/* @param {String} elementID - ID of element*/
+hideElement = elementID => {
+  let element = document.getElementById(elementID);
+
+  element.style.display = 'none';
+};
+
+/* @param {String} element - ID of element
+ * @param {String} element - display style. Defaults to 'block'
+ */
+showElement = (elementID, displayStyle = 'block') => {
+  let element = document.getElementById(elementID);
+
+  element.style.display = displayStyle;
+};
+
+/* @param {String} elem1ID - ID of element to be hidden
+ * @param {String} elem2ID - ID of element to be revealed
+ * @param {String} displayStyle - display style; defualts to 'block'
+ */
+toggleElements = (elem1ID, elem2ID, displayStyle = 'block') => {
+  hideElement(elem1ID);
+  showElement(elem2ID, displayStyle);
+};
