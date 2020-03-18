887b486b187290d6571ca900c0e352e560f2d731
Cate <ceh2181@columbia.edu>
2020-03-17 20:08:20 -0400

    Smooth fade transitions on about content toggle
    - Make fade animation keyframes globally accessible
    - Rename fade js animations for accuracy
    - Improve documentation

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,18 +1,23 @@
-/* @param {String} elementID - ID of element*/
-hideElement = elementID => {
+/* Removes element with display none & slow fade
+ * @param {String} elementID - ID of element
+ * @param {String} duration - duration of fade animation
+ */
+removeElementWithFade = (elementID, duration = '1s') => {
   let element = document.getElementById(elementID);
 
-  element.style.animation = 'fadeOut 1s';
+  element.style.animation = `fadeOut ${duration} ease-in`;
   element.style.display = 'none';
 };
 
-/* @param {String} element - ID of element
+/* Slowly fades element into view
+ * @param {String} element - ID of element
  * @param {String} element - display style. Defaults to 'block'
+ * @param {String} duration - duration of fade animation
  */
-showElement = (elementID, displayStyle = 'block') => {
+fadeInElement = (elementID, displayStyle = 'block', duration = '1s') => {
   let element = document.getElementById(elementID);
 
-  element.style.animation = 'fadeIn 1s';
+  element.style.animation = `fadeIn ${duration} ease-in`;
   element.style.display = displayStyle;
 };
 
@@ -21,6 +26,6 @@ showElement = (elementID, displayStyle = 'block') => {
  * @param {String} displayStyle - display style; defualts to 'block'
  */
 toggleElements = (elem1ID, elem2ID, displayStyle = 'block') => {
-  hideElement(elem1ID);
-  showElement(elem2ID, displayStyle);
+  removeElementWithFade(elem1ID);
+  fadeInElement(elem2ID, displayStyle);
 };
