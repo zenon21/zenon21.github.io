b484f47bac1e0cfc93b19ed8f2fae9f937eb2883
Cate <ceh2181@columbia.edu>
2020-08-30 16:09:12 -0400

    WIP: Layout mobile nav overlay
    - Position close button & nav links
    - Ensure nav overlay remains fixed height & does not scroll by setting
      overflow: hidden on body when overlay is open

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
+/**
+ * Opens mobile nav overlay and fixes body in place so it won't scroll while
+ * nav overlay is open.
+ */
 function toggleMobileNav() {
+  const body = document.querySelector('body');
   const overlay = document.querySelector('.mobile-nav__overlay');
 
   overlay.classList.toggle('mobile-nav--open');
+  body.classList.toggle('overflow-hidden');
 }
