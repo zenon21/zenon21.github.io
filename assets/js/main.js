47bfbe37951a820ad805b6d9dc6dbfbde187462e
Cate <ceh2181@columbia.edu>
2020-08-30 04:19:15 -0400

    WIP: Implement function to toggle mobile nav overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,5 @@
-let toggleAboutLinks = document.getElementsByClassName('toggle-about__link');
+function toggleMobileNav() {
+  const overlay = document.querySelector('.mobile-nav-overlay');
 
-for (const link of toggleAboutLinks) {
-  link.addEventListener('click', toggleAbouts);
-}
-
-function toggleAbouts() {
-  let aboutMe = document.getElementById('about-me');
-  let aboutSite = document.getElementById('about-site');
-
-  aboutMe.classList.toggle('hidden');
-  aboutSite.classList.toggle('hidden');
+  overlay.classList.toggle('mobile-nav-open');
 }
